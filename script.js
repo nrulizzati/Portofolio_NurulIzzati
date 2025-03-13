b2170d83ac13e73bb481b7712fe3e574fa754a10
nurizz <166016905+nrulizzati@users.noreply.github.com>
2025-03-13 18:53:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidebar").classList.add("active");
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").classList.remove("active");
+}
